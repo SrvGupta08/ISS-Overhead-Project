719097df5b712f60f2a2c6d677573ef7c0ab0985
Sourav Gupta <srv.iem08@gmail.com>
2025-06-24 11:16:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+''' Check the current location of International Space System (ISS) and if it is near your location during the night time then send yourself an
+Email to look up. '''
+
+import requests
+from datetime import datetime
+import smtplib
+
+MY_LATITUDE = 22.585905
+MY_LONGITUDE = 88.326520
+MY_EMAIL = "guptasourav0809@gmail.com"
+MY_PASSWORD = "yyirccpuofuaveoo"
+
+time_now = datetime.now()
+
+response = requests.get(url = "http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+
+iss_latitude = data["iss_position"]["latitude"]
+iss_longitude = data["iss_position"]["longitude"]
+
+print(iss_latitude, iss_longitude)
+
+def my_location():
+    if (MY_LATITUDE - 5) <= iss_latitude <= (MY_LATITUDE + 5) and (MY_LONGITUDE - 5) <= iss_longitude <= (MY_LONGITUDE + 5):
+        return time_now.hour
+    else:
+        return False
+
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user = MY_EMAIL, password = MY_PASSWORD)
+        connection.sendmail(
+            from_addr = MY_EMAIL,
+            to_addrs = "souravgupta0809@yahoo.com",
+            msg = "Subject: Look Up\n\nLook up in the sky" 
+        )
+
+if my_location() >= 17 or my_location() <= 5:
+    send_email()
+else:
+    pass
